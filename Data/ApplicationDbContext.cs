caf101fc555348e10aee466913bb179225d1e14d
Igor Telheiro <54379860+igortelheiro@users.noreply.github.com>
2021-03-18 00:49:05 -0300

    first commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Blockbuster.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blockbuster.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blockbuster.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string DirectorName { get; set; }
+        [Required]
+        public int ReleaseYear { get; set; }
+    }
+}
